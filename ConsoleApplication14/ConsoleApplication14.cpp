af772eaf43a4773f256bb938121d286f05f24085
vlad07072002li@gmail.com <vlad07072002li@gmail.com>
2020-06-04 12:37:23 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication14/ConsoleApplication14.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <list>
+int main()
+{
+	srand((unsigned)time(0));
+
+	std::list<int> Li;
+	for (int i = 0; i < 50; i++)
+		Li.push_back(rand() % 100 - 50);
+
+	std::list<int>::iterator p = Li.begin();
+	for (; p != Li.end(); ++p)
+		std::cout << *p << ' ';
+	std::cout << std::endl;
+
+	for (p = Li.begin(); p != Li.end();)
+	{
+		if (*p < 0)
+		{
+			Li.insert(++p, 10);
+		}
+		else
+			++p;
+	}
+
+	std::cout << std::endl;
+	for (p = Li.begin(); p != Li.end(); ++p)
+		std::cout << *p << ' ';
+	std::cout << std::endl;
+
+	system("pause");
+}
